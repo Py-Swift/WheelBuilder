146cc323ac083479943f7968fbbdea415d0861ed
Py-Swift <psychowaspx@gmail.com>
2025-11-09 17:37:42 +0100

    Update Bcrypt to conform to CiWheelProtocol
    Changed Bcrypt to implement CiWheelProtocol instead of MaturinWheelProtocol and added a patches() method returning the bcrypt patch URL.

--- Sources/CiWheels/Maturin/Bcrypt.swift
+++ Sources/CiWheels/Maturin/Bcrypt.swift
@@ -4,8 +4,13 @@
 //
 import PlatformInfo
 import PathKit
+import Foundation
 
 @WheelClass
-public final class Bcrypt: MaturinWheelProtocol {
+public final class Bcrypt: CiWheelProtocol {
 
+    
+    public func patches() -> [URL] {
+        ["https://raw.githubusercontent.com/Py-Swift/LibraryPatches/refs/heads/master/bcrypt.patch"]
+    }
 }
