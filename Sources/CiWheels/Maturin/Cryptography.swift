a073174f229b9b2f15a639bda9d459afa6845016
psychowasp <psychowaspx@gmail.com>
2026-04-27 18:59:21 +0200

    fix: pre-install cffi+setuptools for cryptography iOS no-isolation build

--- Sources/CiWheels/Maturin/Cryptography.swift
+++ Sources/CiWheels/Maturin/Cryptography.swift
@@ -14,6 +14,11 @@ public final class Cryptography: MaturinWheelProtocol {
     public func env() throws -> [String : String] {
         var env = try maturin_env()
         env["OPENSSL_DIR"] = (root + "openssl/\(platform.sdk_arch)").string
+        if platform.get_sdk() != .android {
+            // cryptography's pyproject.toml requires cffi + setuptools in addition to maturin.
+            // With --no-isolation these must all be pre-installed by CIBW_BEFORE_BUILD.
+            env["CIBW_BEFORE_BUILD"] = "pip install maturin cffi setuptools"
+        }
         return env
     }
     
